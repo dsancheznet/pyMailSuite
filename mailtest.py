d79339a8bad624d06d4449c04f5b29cd48609b74
Dominik Sánchez Meckenstock <dominik@ElementaryOS.fritz.box>
2022-04-11 17:27:54 +0100

    REMOVAL: removed test files for private testing purposes.

--- mailtest.py
+++
@@ -1,9 +0,0 @@
-from email.message import EmailMessage
-
-myMessage = EmailMessage()
-
-myMessage['From']="test@gmail.com"
-myMessage['To']='test2@gmail.com'
-myMessage['Subject']="Hola"
-myMessage.set_content( "Cuerpo del Mensaje ")
-print( myMessage.as_string())
